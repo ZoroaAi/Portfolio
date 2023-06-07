9532a0b8eafd6c4c4f66c8ff51f0be37426e4124
ZoroAi <sauravkc456@gmail.com>
2023-06-07 22:28:24 +0100

    basic navbar + logo

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <div className="app">
+      <Navbar/>
       <h1 class="intro">
         Hi! I'm Saurav.
       </h1>
---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,24 @@
+import '../styles/navbar.scss'
+import logo from '../images/Logo.png'
+
+function Navbar(){
+    return(
+        <div className="navbar">
+            <div className="navbar-top">
+                <img src={logo} alt="logo" className="logo"/>
+            </div>
+            <div className="navbar-middle">
+                <ul>
+                    <li><a href="#intro">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+            <div className="navbar-bottom">
+                <button type="button">Email me</button>
+            </div>
+        </div>
+    )
+}
+export default Navbar;

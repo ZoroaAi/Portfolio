57e6c48b9576ef1db5be64ee0ca21bedca5ae526
ZoroAi <sauravkc456@gmail.com>
2023-06-07 17:16:54 +0100

    set up react

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1 class="intro">
+        Hi! I'm Saurav.
+      </h1>
+    </div>
+  );
+}
+
+export default App;

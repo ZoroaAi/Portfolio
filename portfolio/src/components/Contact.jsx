15a66bacfed78eb0edc45d61cde244be7f0014c7
ZoroAi <sauravkc456@gmail.com>
2023-06-09 13:12:26 +0100

    Added send email function

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -1,41 +1,44 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 import '../styles/contact.scss';
 
 function Contact() {
-    const [title, setTitle] = useState("");
-    const [email, setEmail] = useState("");
+    const [fromName, setFromName] = useState("");
     const [message, setMessage] = useState("");
+    const [email, setEmail] = useState("");
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const response = await fetch('/contact', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ title, email, message })
+    const form = useRef();
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        const service = process.env.REACT_APP_SERVICE_ID;
+        const template = process.env.REACT_APP_TEMPLATE_ID;
+        const publick = process.env.REACT_APP_PUBLIC_KEY;
+
+        emailjs.sendForm(service,
+            template,
+            form.current,
+            publick)
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
         });
-
-        if (response.ok) {
-            alert("Message sent!");
-            setTitle("");
-            setEmail("");
-            setMessage("");
-        } else {
-            alert("Message failed to send.");
-        }
-    };
+    }
 
     return (
         <div>
             <div className="contact-wrapper">
                 <div className="contact-inside-wrapper">
                     <h2>Contact Me</h2>
-                    <form onSubmit={handleSubmit} className='email-form'>
+                    <form ref={form} onSubmit={sendEmail} className='email-form'>
                         <div className="title-email">
-                            <input type="text" placeholder="Title" value={title} onChange={e => setTitle(e.target.value)} required/>
-                            <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} required/>
+                            <input type="text" placeholder="Your Name" value={fromName} name='from_name' onChange={e => setFromName(e.target.value)} required/>
+                            <input type="email" placeholder="Email" value={email} name='email' onChange={e => setEmail(e.target.value)} required/>
                         </div>
-                        <textarea placeholder="Message" value={message} onChange={e => setMessage(e.target.value)}  required></textarea>
+                        <textarea placeholder="Message" value={message} name='message' onChange={e => setMessage(e.target.value)}  required></textarea>
                         <button type="submit">Submit</button>
                     </form>
                 </div>

f1e3803354f74b9b455bc663200031cbe1c853e0
ZoroAi <sauravkc456@gmail.com>
2023-06-28 22:56:35 +0100

    added room with lighting and fog

--- portfolio/src/components/AboutMe.jsx
+++ portfolio/src/components/AboutMe.jsx
@@ -1,24 +1,79 @@
-import { Suspense, useEffect, useState} from 'react'
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Preload, useGLTF, Box } from '@react-three/drei';
+import { Suspense, useEffect, useRef, useState} from 'react'
+import { Canvas, useThree } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import { DirectionalLightHelper, Object3D, SpotLightHelper } from 'three';
+import * as THREE from 'three';
 
-import CanvasLoader from './Loader';
+
+import Loader from './Loader';
 
 import '../styles/about.scss';
 import { SectionWrapper } from '../hoc';
 
 const Room = () => {
-    const room = useGLTF('/models/room/room_model.glb')
-    room.scene.scale.set(6, 6, 6);
+    const room = useGLTF('/models/room/room_model.glb');
+    const dirLightRef = useRef();
+    const spotLightRef = useRef();
+    const deskTarget = new Object3D();
+    deskTarget.position.set(-3, 0, -4);
+
+    const { scene } = useThree();
+
+    useEffect(() => {
+        if (dirLightRef.current) { 
+            const dirHelper = new DirectionalLightHelper(dirLightRef.current);
+            scene.add(dirHelper);
+        }
+
+        if (spotLightRef.current) { 
+            // const spotHelper = new SpotLightHelper(spotLightRef.current);
+            // scene.add(spotHelper);
+            spotLightRef.current.target = deskTarget;
+            scene.add(spotLightRef.current.target);
+        }
+        scene.fog = new THREE.Fog('#e9d3c5', 10, 250)
+
+    }, [scene]);
+
     return (
         <mesh>
-        <hemisphereLight 
-            intensity={0.15} 
-            groundColor={"black"}
+        <ambientLight 
+            intensity={0.3}
         />
-        <pointLight intensity={1}/>
+        <directionalLight 
+            ref={dirLightRef}
+            color={'#FF8C00'}
+            intensity={2}
+            position={[-1.5, 3, -10]}
+            castShadow
+        />
+        <spotLight 
+            ref={spotLightRef}
+            color={'#FF8C00'}
+            position={[-1.5, 3, -10]} 
+            angle={3} 
+            penumbra={0.5} 
+            intensity={0.8} 
+            castShadow 
+        />
+        <spotLight 
+            ref={spotLightRef}
+            color={'#c1dbf1'}
+            position={[-5.2, 1.5, -6.7]} 
+            angle={3} 
+            penumbra={0.5} 
+            intensity={0.8} 
+            castShadow 
+        />
+        <pointLight 
+            color={'#d8af93'}
+            intensity={0.4}
+            position={[5, 3, -10]}
+        />
         <primitive 
             object={room.scene}
+            scale={5}
+            position={[0,-3.25,-1.5]}
         />
         </mesh>
     )
@@ -27,16 +82,18 @@ const Room = () => {
 const RoomModel = () => {
     return (
         <Canvas 
-        frameLoop='demand'
-        shadows
-        camera={{position: [20,3,5], fov: 25}}
-        gl={{preserveDrawingBuffer: true}}
+            frameLoop='demand'
+            shadows
+            camera={{position: [40,70,30], fov: 14}}
+            gl={{preserveDrawingBuffer: true}}
         >
-        <Suspense fallback={<CanvasLoader/>} >
+        <Suspense fallback={<Loader/>} >
             <OrbitControls 
             enableZoom= {false}
             maxPolarAngle= {Math.PI / 2}
-            minPolarAngle= {Math.PI / 2}
+            minPolarAngle= {Math.PI / 4}
+            maxAzimuthAngle= {Math.PI / 4}
+            minAzimuthAngle= {Math.PI / 4}
             />
             <Room />
         </Suspense>
---
+++ portfolio/src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import { Html, useProgress } from '@react-three/drei';
+
+const Loader = () => {
+    const { progress } = useProgress();
+    return (
+        <Html>
+            <span className='canvas_load'></span>
+            <p className='progress_num'>{progress.toFixed(2)} %</p>
+        </Html>
+    )
+}
+
+export default Loader;

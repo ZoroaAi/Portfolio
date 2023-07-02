61d94d390b12aef1474c1dbc6fa3dde0ec4fcc16
ZoroAi <sauravkc456@gmail.com>
2023-07-02 13:11:33 +0100

    LOADER FINISHED

--- portfolio/src/components/Loaders/PageLoader.jsx
+++ portfolio/src/components/Loaders/PageLoader.jsx
@@ -1,7 +1,10 @@
-import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { motion, animate } from "framer-motion";
 
 import '../../styles/loader.scss';
 import { clouds } from "../../assets";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowPointer } from '@fortawesome/free-solid-svg-icons'
 
 function Cloud({speed, i}) {
     return (
@@ -26,72 +29,144 @@ function Cloud({speed, i}) {
     );
 }
 
+const PageLoader = ({loading}) => {
+    const [loadingProgress, setLoadingProgress] = useState(0);
+    const [isVisible, setIsVisible] = useState(false);
 
-const PageLoader = ({isLoading}) => {
+    useEffect(() => {
+        setIsVisible(true);
+
+        if (loading) {
+        const controls = animate(0, 100, {
+            duration: 3,
+            onUpdate(value) {
+            setLoadingProgress(value);
+            },
+        });
+
+        return controls.stop;
+        } else {
+        setLoadingProgress(100);
+        }
+    }, [loading]);
+
     return (
         <div className="loader_wrapper">
             <motion.div className="computer">
-                <div className="bevel">
-                    <div className="screen">
-                        <Cloud speed={2} i={0} />
-                        <Cloud speed={3} i={1} />
-                        <Cloud speed={3} i={2} />
-                        <Cloud speed={3} i={3} />
-                        <span>Loading</span>
-                        <div className="ground"></div>
-                        <div className="hill-1"></div>
-                        <div className="hill-2"></div>
-                        <div className="hill-3"></div>
+                <motion.div
+                    initial= {{ opacity: 0, scale:0.8 }}
+                    animate={{ opacity: 1, scale: [1,1.1,1]}}
+                    transition={{
+                        duration: 0.7,
+                        delay: 0.2,
+                        ease: 'easeOut'
+                    }}
+                    style={{ visibility: isVisible ? 'visible' : 'hidden' }}
+                >
+                    <div className="bevel">
+                        <div className="screen">
+                            <Cloud speed={2} i={0} />
+                            <Cloud speed={3} i={1} />
+                            <Cloud speed={3} i={2} />
+                            <Cloud speed={3} i={3} />
+                            <span>Loading...</span>
+                            <div className="ground"></div>
+                            <div className="hill-1"></div>
+                            <div className="hill-2"></div>
+                            <div className="hill-3"></div>
+                            <FontAwesomeIcon 
+                                className="pointer" 
+                                icon={faArrowPointer}
+                            />
+                        </div>
                     </div>
-                </div>
+                </motion.div>
                 <div className="keyboard_mouse">
-                    <div className="keyboard">
-                        <div className="row">
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
+                    <motion.div
+                        initial= {{ opacity: 0, scale:0.8 }}
+                        animate={{ opacity: 1, scale: [1,1.1,1]}}
+                        transition={{
+                            duration: 0.7,
+                            delay: 0.4,
+                            ease: 'easeOut'
+                        }}
+                        style={{ visibility: isVisible ? 'visible' : 'hidden' }}
+                    >
+                        <div className="keyboard">
+                            <div className="row">
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                            </div>
+                            <div className="row">
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                            </div>
+                            <div className="row">
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                                <div className="key"></div>
+                            </div>
                         </div>
-                        <div className="row">
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
+                    </motion.div>
+                    <motion.div
+                        initial= {{ opacity: 0, scale:0.8 }}
+                        animate={{ opacity: 1, scale: [1,1.1,1]}}
+                        transition={{
+                            duration: 0.7,
+                            delay: 0.6,
+                            ease: 'easeOut'
+                        }}
+                        style={{ visibility: isVisible ? 'visible' : 'hidden' }}
+                    >
+                        <div className="mouse">
+                            <div className="mouse_scroll"></div>
                         </div>
-                        <div className="row">
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                            <div className="key"></div>
-                        </div>
-                    </div>
-                    <div className="mouse">
-                        <div className="mouse_scroll"></div>
-                    </div>
+                    </motion.div>
+                </div>
+                <div className="bar_wrapper">
+                    <motion.div
+                        className="loading_bar"
+                        initial= {{ opacity: 0, scale:0.8 }}
+                        animate={{ opacity: 1, scale: [1,1.1,1]}}
+                        transition={{
+                            duration: 0.7,
+                            delay: 0.8,
+                            ease: 'easeOut'
+                        }}
+                        style={{
+                            visibility: isVisible ? 'visible' : 'hidden',
+                            width: `${loadingProgress}%`,
+                        }}
+                    />
                 </div>
             </motion.div>
         </div>

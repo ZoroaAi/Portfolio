ddbf47df9e2bcf6255dbebc6fae3c5dab2319424
ZoroAi <sauravkc456@gmail.com>
2023-07-08 22:32:40 +0100

    nav not workinggggg

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { Link as ScrollLink} from 'react-scroll';
-import { motion, useCycle, AnimatePresence } from 'framer-motion';
+import { motion, useCycle, AnimatePresence, Transition, SVGMotionProps } from 'framer-motion';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes, faBars, faFileArrowDown } from '@fortawesome/free-solid-svg-icons';
@@ -11,98 +11,161 @@ import { navLinks } from '../constants/constant';
 import { logo, cv } from '../assets';
 import { mobileItemVariant, mobileNavVariant, sideBarVariant } from '../utils/motion';
 
-const Path = props => (
-    <motion.path
-      fill="transparent"
-      strokeWidth="3"
-      stroke="hsl(0, 0%, 18%)"
-      strokeLinecap="round"
-      {...props}
-    />
-);
-  
-const MenuToggle = ({ toggle }) => (
-    <button onClick={toggle} className='menu_toggle_button'>
-      <svg width="23" height="23" viewBox="0 0 23 23">
-        <Path
-          variants={{
-            closed: { d: "M 2 2.5 L 20 2.5" },
-            open: { d: "M 3 16.5 L 17 2.5" }
-          }}
-        />
-        <Path
-          d="M 2 9.423 L 20 9.423"
-          variants={{
-            closed: { opacity: 1 },
-            open: { opacity: 0 }
-          }}
-          transition={{ duration: 0.1 }}
-        />
-        <Path
-          variants={{
-            closed: { d: "M 2 16.346 L 20 16.346" },
-            open: { d: "M 3 2.5 L 17 16.346" }
-          }}
-        />
-      </svg>
-    </button>
-);
+
+const MenuButton = ({
+    isOpen = false,
+    width = 24,
+    height = 24,
+    strokeWidth = 1,
+    color = "#000",
+    transition = null,
+    lineProps = null,
+    onClick,
+    ...props
+    }) => {
+        const variant = isOpen ? "opened" : "closed";
+        const top = {
+        closed: {
+            rotate: 0,
+            translateY: 0
+        },
+        opened: {
+            rotate: 45,
+            translateY: 2
+        }
+        };
+        const center = {
+        closed: {
+            opacity: 1
+        },
+        opened: {
+            opacity: 0
+        }
+        };
+        const bottom = {
+        closed: {
+            rotate: 0,
+            translateY: 0
+        },
+        opened: {
+            rotate: -45,
+            translateY: -2
+        }
+        };
+        lineProps = {
+            stroke: color,
+            strokeWidth: Number(strokeWidth),
+            vectorEffect: "non-scaling-stroke",
+            initial: "closed",
+            animate: variant,
+            transition,
+            ...lineProps
+        };
+        const unitHeight = 4;
+        const unitWidth = (unitHeight * Number(width)) / Number(height);
+    
+        return (
+        <motion.svg
+            viewBox={`0 0 ${unitWidth} ${unitHeight}`}
+            overflow="visible"
+            preserveAspectRatio="none"
+            width={width}
+            height={height}
+            className={'burger'}
+            onClick={onClick}
+            {...props}
+        >
+            <motion.line
+            x1="0"
+            x2={unitWidth}
+            y1="0"
+            y2="0"
+            variants={top}
+            {...lineProps}
+            />
+            <motion.line
+            x1="0"
+            x2={unitWidth}
+            y1="2"
+            y2="2"
+            variants={center}
+            {...lineProps}
+            />
+            <motion.line
+            x1="0"
+            x2={unitWidth}
+            y1="4"
+            y2="4"
+            variants={bottom}
+            {...lineProps}
+            />
+        </motion.svg>
+    );
+};
 
 const navVariants = {
     open: { opacity: 1, height: "auto" },
     closed: { opacity: 0, height: 0 }
 };
 
-const MobileMenu = ({isOpen, toggle}) => {
+const MobileMenu = ({isOpen, toggle, setActive}) => {
     return (
-        <AnimatePresence>
-            {isOpen && (
-                <motion.div
-                    className="navbar-collapse"
-                    variants={navVariants}
-                    initial="closed"
-                    animate="open"
-                    exit="closed"
-                >
-                    <ul>
-                        {navLinks.map((link) => (
-                            <li key={link.id}> 
-                                <ScrollLink
-                                    activeClass="active"
-                                    to={link.id}
-                                    spy={true}
-                                    smooth={true}
-                                    offset={-20}
-                                    duration={500}
-                                    onClick={toggle}
-                                >
-                                    {link.title}
-                                </ScrollLink>
+        <>
+            <MenuButton isOpen={isOpen} onClick={() => {toggle()}} />
+            <AnimatePresence>
+                {isOpen && (
+                    <motion.div
+                        className="navbar-collapse"
+                        variants={navVariants}
+                        initial="closed"
+                        animate="open"
+                        exit="closed"
+                    >
+                        <ul>
+                            {navLinks.map((link) => (
+                                <li key={link.id}>
+                                    <ScrollLink
+                                        activeClass="active"
+                                        to={link.id}
+                                        spy={true}
+                                        smooth={true}
+                                        offset={-20}
+                                        duration={500}
+                                        onClick={() => {setActive(link.title)}}
+                                    >
+                                        {link.title}
+                                    </ScrollLink>
+                                </li>
+                            ))}
+                            <li>
+                                <a href='_blank' download={cv}>
+                                    <button className='download_button'>
+                                        <FontAwesomeIcon icon={faFileArrowDown} />
+                                        <span>Download My CV</span>
+                                    </button>
+                                </a>
                             </li>
-                        ))}
-                        <li>
-                            <a href='_blank' download={cv}>
-                                <button className='download_button'>
-                                    <FontAwesomeIcon icon={faFileArrowDown} />
-                                    <span>Download My CV</span>
-                                </button>
-                            </a>
-                        </li>
-                    </ul>
-                </motion.div>
-            )}
-        </AnimatePresence>
+                        </ul>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </>
     )
 }
 
 function Navbar(){
-    const [isOpen, toggleOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
     const [active, setActive] = useState("");
     const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
     const handleResize = () => {
         setIsMobile(window.innerWidth <= 768);
     };
+
+    const toggleOpen = () => {
+        console.log('Triggered Open/Close Menu');
+        setIsOpen(!isOpen);
+    }
 
     useEffect(() => {
         window.addEventListener("resize", handleResize);
@@ -124,8 +187,7 @@ function Navbar(){
             <div className="navbar_right">
             {isMobile ? (
                 <div className="navbar_right">
-                    <MenuToggle toggle={() => toggleOpen(!isOpen)} />
-                    <MobileMenu isOpen={isOpen} toggle={() => toggleOpen(!isOpen)} />
+                    <MobileMenu isOpen={isOpen} toggle={() => {toggleOpen()}} setActive={setActive} />
                 </div>
             ) : (
                 <div className="navbar_right">

14b55e25691ec2dbedc9018d9bec3c2fad13a118
ZoroAi <sauravkc456@gmail.com>
2023-07-13 21:15:53 +0100

    fixed about and exp section

--- portfolio/src/components/AboutMe.jsx
+++ portfolio/src/components/AboutMe.jsx
@@ -128,10 +128,10 @@ function AboutMe(){
             <motion.div className="about-info" variants={textVariant()}>
                 <p className='about_subText'>Who I Am</p>
                 <h2>About Me.</h2>
-                <p>Hello, I'm Saurav KC, soon to be graduating with a degree in Computer Science from the University of Portsmouth. My journey in this diverse field has led me to discover a increasing appreciation for the art and science of web design and development.
+                <motion.p>Hello, I'm Saurav KC, soon to be graduating with a degree in Computer Science from the University of Portsmouth. My journey in this diverse field has led me to discover a increasing appreciation for the art and science of web design and development.
                 <br/><br/>This passion isn't just theoretical, it has driven me to explore a wide variety of tech stacks and apply them in practical settings, making each project a unique blend of creativity and functionality.
                 Feel free to explore more about me and my work. I'm excited about the opportunities ahead and I welcome any conversations about how my passion and skills could meet your needs.
-                </p>
+                </motion.p>
             </motion.div>
             <div className="about-img">
                 <motion.div variants={fadeIn('up', 'spring',0.8,0.75)}>
